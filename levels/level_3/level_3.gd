d63058148a4788c6e0cac7f1b78b4b30df5dc3aa
Fawwaz <faw.edsa@gmail.com>
2026-05-29 16:49:41 +0700

    add new background, level title, deathzone, adn death message

--- levels/level_3/level_3.gd
+++ levels/level_3/level_3.gd
@@ -2,11 +2,18 @@ extends Node2D
 
 @onready var button_laser = %ButtonLaser 
 @onready var laser_gate = %LaserGate     
+@onready var button_pillar = %ButtonPillar
+@onready var laser_gate_pillar = %LaserGatePillar
 @onready var terminal = %Terminal 
 @onready var bridge_manager = %BridgeManager
 
 func _ready() -> void:
 	if button_laser and laser_gate:
 		button_laser.button_toggled.connect(laser_gate._on_button_toggled)
+	if button_pillar and laser_gate_pillar:
+		button_pillar.button_toggled.connect(_on_button_pillar_toggled)
 	if terminal and bridge_manager:
 		terminal.terminal_activated.connect(bridge_manager._on_terminal_activated)
+
+func _on_button_pillar_toggled(is_on: bool) -> void:
+	laser_gate_pillar.set_door_status(not is_on)
